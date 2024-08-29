dd730b5fda8e23257856ed99adf3f37d281ef9b2
selim-jo <skyblue.777@daum.net>
2024-08-29 22:30:53 +0900

    [add] 스프링 MVC 2편-타임리프 기본 기능(주석(실습))

--- 1. 타임리프-기본 기능/실습/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ 1. 타임리프-기본 기능/실습/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -137,4 +137,11 @@ public class BasicController {
 
         model.addAttribute("users", list);
     }
+
+    // 주석
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
 }
