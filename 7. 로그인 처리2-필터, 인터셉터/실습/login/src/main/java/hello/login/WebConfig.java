c2790745e56dd0ce4ff8c60d8a487420aa289ccf
selim-jo <skyblue.777@daum.net>
2024-10-15 18:53:05 +0900

    [add] 스프링 MVC 2편-로그인 처리2-필터, 인터셉터(서블릿 필터 - 인증 체크(실습))

--- 7. 로그인 처리2-필터, 인터셉터/실습/login/src/main/java/hello/login/WebConfig.java
+++ 7. 로그인 처리2-필터, 인터셉터/실습/login/src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig {
 
+    // 로그 필터
     @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean();
@@ -20,5 +22,18 @@ public class WebConfig {
         // 필터를 적용할 URL 패턴을 지정함. 한번에 여러 패턴을 지정할 수 있음.
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
+    }
+
+    // 로그인 체크 필터
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        // 로그인 필터 등록
+        filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+        // 로그 필터에 이어서 두번째로
+        filterFilterRegistrationBean.setOrder(2);
+        // 모든 요청에 로그인 필터 적용x
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        return filterFilterRegistrationBean;
     }
 }
