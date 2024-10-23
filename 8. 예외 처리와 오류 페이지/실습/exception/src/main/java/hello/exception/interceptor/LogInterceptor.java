5f6bf61453e2d8dfc0c20ca2e5ef7b5605d1c139
selim-jo <skyblue.777@daum.net>
2024-10-23 21:09:32 +0900

    [add] 스프링 MVC 2편-예외 처리와 오류 페이지(스프링 부트-오류 페이지1(실습))

--- 8. 예외 처리와 오류 페이지/실습/exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
+++ 8. 예외 처리와 오류 페이지/실습/exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
@@ -13,8 +13,7 @@ public class LogInterceptor implements HandlerInterceptor {
     public static final String LOG_ID = "logId";
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse
-            response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
         String uuid = UUID.randomUUID().toString();
         request.setAttribute(LOG_ID, uuid);
@@ -31,9 +30,9 @@ public class LogInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         String requestURI = request.getRequestURI();
         String logId = (String)request.getAttribute(LOG_ID);
-        log.info("RESPONSE [{}][{}][{}]", logId, request.getDispatcherType(),
-                requestURI);
+        log.info("RESPONSE [{}][{}][{}]", logId, request.getDispatcherType(), requestURI);
         if (ex != null) {
             log.error("afterCompletion error!!", ex);
         }
-    } }
+    }
+}
