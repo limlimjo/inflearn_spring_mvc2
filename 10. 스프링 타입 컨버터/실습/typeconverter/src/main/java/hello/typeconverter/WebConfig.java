82551d53f071c843ff3892852fc7a0592b12bb82
selim-jo <skyblue.777@daum.net>
2024-11-06 18:44:15 +0900

    [add] 스프링 MVC 2편-스프링 타입 컨버터(스프링에 Converter 적용하기(실습))

---
+++ 10. 스프링 타입 컨버터/실습/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,22 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // 컨버터를 추가하면 추가한 컨버터가 기본 컨버터보다 높은 우선순위를 가짐
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
